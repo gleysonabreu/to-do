a7de8cee12e78a25bd0bf445700a7ad9c51b4261
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-09-04 20:55:45 -0300

    feat: create account controller

---
+++ src/infra/http/controllers/create-account.controller.ts
@@ -0,0 +1,54 @@
+import { RegisterUserUseCase } from '@/domain/user/use-cases/register-user';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+import { UserAlreadyExistsError } from '@/domain/user/use-cases/errors/user-already-exists-error';
+
+const createAccountBodySchema = z.object({
+  firstName: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(5).max(15),
+  lastName: z.string().min(2),
+  username: z.string().min(4),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller('/users')
+export class CreateAccount {
+  constructor(private registerUser: RegisterUserUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { email, firstName, lastName, password, username } = body;
+
+    const result = await this.registerUser.execute({
+      email,
+      firstName,
+      lastName,
+      password,
+      username,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case UserAlreadyExistsError:
+          throw new ConflictException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { Database } from '../database/database.module';
 import { CryptographyModule } from '../cryptography/cryptography.module';
+import { CreateAccount } from './controllers/create-account.controller';
+import { RegisterUserUseCase } from '@/domain/user/use-cases/register-user';
 
 @Module({
   imports: [Database, CryptographyModule],
+  controllers: [CreateAccount],
+  providers: [RegisterUserUseCase],
 })
 export class HttpModule {}
