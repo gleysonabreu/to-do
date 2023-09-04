b156eb827ee8daa95d9bfffe34083f5567cf603d
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-09-04 19:17:09 -0300

    lint files

--- src/core/types/optional.ts
+++ src/core/types/optional.ts
@@ -2,4 +2,4 @@
  * Make some property optional on type
  **/
 
-export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
