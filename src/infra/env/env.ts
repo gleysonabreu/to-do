110dd50350b6cd5062605fb94ec7dd076e9985f8
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-09-04 13:22:54 -0300

    feat: add setup docker-compose

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.coerce.number(),
+  DB_DATABASE: z.string(),
   DATABASE_URL: z.string().url(),
 });
 
