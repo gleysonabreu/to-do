ed4856006a20eb46618ec298823f2b08c1a489a8
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-09-04 19:12:26 -0300

    feat: create prisma users repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersRepository } from 'src/domain/user/repositories/users-repository';
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+    PrismaUsersRepository,
+  ],
+  exports: [PrismaService, UsersRepository, PrismaUsersRepository],
 })
 export class Database {}
---
+++ src/infra/database/prisma/repositories/prisma-users-repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../../../../domain/user/repositories/users-repository';
+import { User } from 'src/domain/user/entities/user';
+import { PrismaService } from '../prisma.service';
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper';
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return PrismaUserMapper.toDomain(user);
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return PrismaUserMapper.toDomain(user);
+  }
+
+  async create(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user);
+
+    await this.prisma.user.create({ data });
+  }
+}
