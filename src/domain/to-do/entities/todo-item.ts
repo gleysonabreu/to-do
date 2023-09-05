33aeb7bf6289199713895b6853ddd41b5bfa8547
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-09-05 16:00:06 -0300

    feat: create todo-item and todo entities

---
+++ src/domain/to-do/entities/todo-item.ts
@@ -0,0 +1,56 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+
+interface TodoItemProps {
+  name: string;
+  description?: string | null;
+  check: boolean;
+  todoId: UniqueEntityID;
+}
+
+export class TodoItem extends Entity<TodoItemProps> {
+  get todoId() {
+    return this.props.todoId;
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  set description(description: string | null) {
+    this.props.description = description;
+  }
+
+  get check() {
+    return this.props.check;
+  }
+
+  done() {
+    this.props.check = true;
+  }
+
+  undone() {
+    this.props.check = false;
+  }
+
+  static create(props: Optional<TodoItem, 'check'>, id?: UniqueEntityID) {
+    const todoItem = new TodoItem(
+      {
+        ...props,
+        check: props.check ?? false,
+      },
+      id,
+    );
+
+    return todoItem;
+  }
+}
---
+++ src/domain/to-do/entities/todo.ts
@@ -0,0 +1,36 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+interface TodoProps {
+  title: string;
+  description?: string | null;
+  userId: UniqueEntityID;
+}
+
+export class Todo extends Entity<TodoProps> {
+  get userId() {
+    return this.props.userId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  set description(description: string | null) {
+    this.props.description = description;
+  }
+
+  static create(props: TodoProps, id?: UniqueEntityID) {
+    const todo = new Todo(props, id);
+
+    return todo;
+  }
+}
