f891ab105681e853ffecae5ef78e2ab1dab94f95
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-10-23 09:51:06 -0300

    user: add setters for all props

--- src/domain/user/entities/user.ts
+++ src/domain/user/entities/user.ts
@@ -10,20 +10,40 @@ export interface UserProps {
 }
 
 export class User extends Entity<UserProps> {
+  set firstName(firstName: string) {
+    this.props.firstName = firstName;
+  }
+
   get firstName() {
     return this.props.firstName;
+  }
+
+  set lastName(lastName: string) {
+    this.props.lastName = lastName;
   }
 
   get lastName() {
     return this.props.lastName;
   }
 
+  set email(email: string) {
+    this.props.email = email;
+  }
+
   get email() {
     return this.props.email;
   }
 
+  set password(password: string) {
+    this.props.password = password;
+  }
+
   get password() {
     return this.props.password;
+  }
+
+  set username(username: string) {
+    this.props.username = username;
   }
 
   get username() {
